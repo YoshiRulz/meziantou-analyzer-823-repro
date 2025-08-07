2877cf0b2ceee573b0bdcb8cb54f5150edbab7fd
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2025-08-07 12:07:51 +1000

    init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public readonly ref struct TestStruct(int n) //: IEquatable<TestStruct>
+{
+	public readonly int N
+		=> n;
+
+	public readonly override bool Equals(object? other)
+		=> false;
+
+	public readonly bool Equals(TestStruct other)
+		=> n == other.N;
+
+	public readonly override int GetHashCode()
+		=> n;
+}
